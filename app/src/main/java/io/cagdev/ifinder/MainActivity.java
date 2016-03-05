ad770fa9acb68d69d96c4fd243fafd647fa99d53
KennyLee <microbean@gmail.com>
2016-03-06 04:59:35 +0800

    get Acticity Data

--- app/src/main/java/io/cagdev/ifinder/MainActivity.java
+++ app/src/main/java/io/cagdev/ifinder/MainActivity.java
@@ -21,19 +21,30 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
+import java.text.DateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
     private static final String TAG = "iFinder";
     private BluetoothAdapter mBluetoothAdapter;
     private static final long SCAN_PERIOD = 120000;
     private Handler mHandler;
     private boolean mScanning;
-    private BluetoothGatt mGatt;
+    private BluetoothGatt mGatt = null;
+    private BluetoothGattService mGattService;
+    private String lastcommand ;
 
     public static final UUID UUID_SERVICE_MILI = UUID.fromString("0000fee0-0000-1000-8000-00805f9b34fb");
     public static final UUID UUID_CHAR_BATTERY = UUID.fromString("0000ff0c-0000-1000-8000-00805f9b34fb");
@@ -44,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
     public static final UUID UUID_NOTIFICATION_HEARTRATE = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb");
     public static final UUID UUID_DESCRIPTOR_UPDATE_NOTIFICATION = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
+    public static final UUID UUID_CHAR_REALTIME_STEPS = UUID.fromString("0000ff06-0000-1000-8000-00805f9b34fb");
+    public static final UUID UUID_CHAR_ACTIVITY_DATA = UUID.fromString("0000ff07-0000-1000-8000-00805f9b34fb");
+    public static final UUID UUID_CHAR_CONTROL_POINT = UUID.fromString("0000ff05-0000-1000-8000-00805f9b34fb");
+    public static final byte MODE_REGULAR_DATA_LEN_MINUTE = 0x1;
+    public static final int BytesPerMinute = 4;
+    public static final byte COMMAND_CONFIRM_ACTIVITY_DATA_TRANSFER_COMPLETE = 0xa;
+    public static final byte COMMAND_FETCH_DATA = 0x6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +70,51 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+        button1 = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+
+        mHandler = new Handler();
+
+        button1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lastcommand = "writeChar";
+                BlueToothDirect();
+            }
+
+        });
+
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lastcommand = "readActivity";
+                BlueToothDirect();
+            }
+
+        });
+
+
+        button3.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lastcommand = "stopsync";
+                BlueToothDirect();
+            }
+
+        });
+
+        button4.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              if (mGatt != null)
+               mGatt.disconnect();
+            }
+
+        });
        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,8 +124,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-        mHandler = new Handler();
-        BlueToothDirect();
+
+
     }
 
     @Override
@@ -110,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
                    /* byte[] VIBRATION_WITH_LED = {1};
                     byte[] ENABLE_REALTIME_STEPS_NOTIFY = {3, 1};
 
-                    BluetoothGattCharacteristic chara = gatt.getService(UUID.fromString("0000fee0-0000-1000-8000-00805f9b34fb")).getCharacteristic(UUID.fromString("0000ff05-0000-1000-8000-00805f9b34fb"));
+                    BluetoothGattCharacteristic chara = gatt.getService(UUID_SERVICE_MILI).getCharacteristic(UUID_CHAR_CONTROL_POINT);
                     if (chara != null) {
                         Log.i("getService", "ENABLE_REALTIME_STEPS_NOTIFY");
                         chara.setValue(ENABLE_REALTIME_STEPS_NOTIFY);
@@ -129,6 +192,83 @@ public class MainActivity extends AppCompatActivity {
             Log.i("onServicesDiscovered", services.toString());
 
             final BluetoothGattService gattService = gatt
+                    .getService(UUID_SERVICE_MILI);
+            if (gattService != null) {
+                Log.i(TAG, "onServicesDiscovered UUID_SERVICE_MILI");
+
+                mGattService = gattService;
+
+               /* BluetoothGattCharacteristic readact = gattService.getCharacteristic(UUID_CHAR_ACTIVITY_DATA);
+                if (readact != null) {
+                    gatt.readCharacteristic(readact);
+                }*/
+
+                if (lastcommand.equals("writeChar")) {
+                    BluetoothGattCharacteristic control = gattService.getCharacteristic(UUID_CHAR_CONTROL_POINT);
+                    if (control != null) {
+                        byte[] GET_ACT = {6};
+                        control.setValue(GET_ACT);
+                        gatt.writeCharacteristic(control);
+                        // gatt.readCharacteristic(control);
+                        return;
+                    }
+                }
+
+                if (lastcommand.equals("stopsync")) {
+                    BluetoothGattCharacteristic control = gattService.getCharacteristic(UUID_CHAR_CONTROL_POINT);
+                    if (control != null) {
+                        byte[] STOP_ACT = new byte[]{0x11};
+                        control.setValue(STOP_ACT);
+                        gatt.writeCharacteristic(control);
+                        return;
+                    }
+                }
+
+
+                //BluetoothGattCharacteristic bch = gattService.getCharacteristic(UUID_CHAR_CONTROL_POINT);
+               // BluetoothGattCharacteristic bch = gattService.getCharacteristic(UUID_CHAR_REALTIME_STEPS);
+                BluetoothGattCharacteristic bch = gattService.getCharacteristic(UUID_CHAR_ACTIVITY_DATA);
+                if (bch != null) {
+                    Log.i(TAG, "BluetoothGattCharacteristic UUID_CHAR_CONTROL_POINT");
+
+                    byte[] VIBRATION_WITH_LED = {1};
+                    byte[] START_HEART_RATE_SCAN = {21, 2, 1};
+                    //byte[] SYNC = {11};
+                   // byte[] GET_ACT = {18,1};
+                    byte[] GET_ACT = {6};
+                    byte[] fetch = new byte[]{COMMAND_FETCH_DATA};
+                    byte[] ENABLE_REALTIME_STEPS_NOTIFY = {3, 1};
+
+                   // bch.setValue(GET_ACT);
+                  //  gatt.writeCharacteristic(bch);
+
+                   // gatt.readCharacteristic(bch);
+
+
+
+
+                        gatt.setCharacteristicNotification(bch, true);
+                        BluetoothGattDescriptor descriptor = bch.getDescriptor(UUID_DESCRIPTOR_UPDATE_NOTIFICATION);
+                        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                        gatt.writeDescriptor(descriptor);
+
+
+                   // bch.setValue(ENABLE_REALTIME_STEPS_NOTIFY);
+                    //gatt.readCharacteristic(bch);
+                    //gatt.writeCharacteristic(bch);
+                    //Log.i(TAG, "BluetoothGattCharacteristic writeCharacteristic OK");
+
+
+                    //Log.i(TAG, "BluetoothGattCharacteristic writeDescriptor OK");
+
+
+
+                }
+
+            }
+        }
+
+           /* final BluetoothGattService gattService = gatt
                     .getService(UUID_SERVICE_HEARTRATE);
             if (gattService != null) {
                 Log.i(TAG, "onServicesDiscovered UUID_SERVICE_HEARTRATE");
@@ -153,19 +293,18 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-               /* BluetoothGattCharacteristic bch2 = gattService.getCharacteristic(UUID_CHAR_HEARTRATE);
+                BluetoothGattCharacteristic bch2 = gattService.getCharacteristic(UUID_CHAR_HEARTRATE);
                 if (bch2 != null) {
                     Log.i(TAG, "BluetoothGattCharacteristic UUID_CHAR_HEARTRATE");
                     byte[] START_HEART_RATE_SCAN = {21, 2, 1};
                     bch2.setValue(START_HEART_RATE_SCAN);
                     gatt.writeCharacteristic(bch2);
-                }*/
+                }
 
             }
 
-           /* gatt.readCharacteristic(services.get(1).getCharacteristics().get
-                    (0));*/
-        }
+           // gatt.readCharacteristic(services.get(1).getCharacteristics().get(0));
+        } */
 
 
 
@@ -174,6 +313,80 @@ public class MainActivity extends AppCompatActivity {
                                          BluetoothGattCharacteristic
                                                  characteristic, int status) {
             Log.i("onCharacteristicRead", characteristic.toString());
+
+            byte[] data = characteristic.getValue();
+
+            Log.i("onCharacteristicRead length", String.valueOf(data.length));
+
+            if (data.length == 4) {
+                int steps = data[3] << 24 | (data[2] & 0xFF) << 16 | (data[1] & 0xFF) << 8 | (data[0] & 0xFF);
+                Log.d(TAG, "steps : " + steps);
+            }
+
+            if (data.length == 11) {
+               // int steps = data[3] << 24 | (data[2] & 0xFF) << 16 | (data[1] & 0xFF) << 8 | (data[0] & 0xFF);
+                //Log.d(TAG, "steps : " + steps);
+                int dataType = data[0];
+
+                GregorianCalendar timestamp = rawBytesToCalendar(data, 1);
+
+                int totalDataToRead = (data[7] & 0xff) | ((data[8] & 0xff) << 8);
+                totalDataToRead *= (dataType == MODE_REGULAR_DATA_LEN_MINUTE) ? BytesPerMinute : 1;
+                Log.d(TAG, "totalDataToRead : " + totalDataToRead);
+
+                // counter of this data block
+                int dataUntilNextHeader = (data[9] & 0xff) | ((data[10] & 0xff) << 8);
+                dataUntilNextHeader *= (dataType == MODE_REGULAR_DATA_LEN_MINUTE) ? BytesPerMinute : 1;
+                Log.d(TAG, "counter data block : " + dataUntilNextHeader);
+
+                Log.d(TAG,"total data to read: " + totalDataToRead + " len: " + (totalDataToRead / BytesPerMinute) + " minute(s)");
+
+                Log.d(TAG,"data to read until next header: " + dataUntilNextHeader + " len: " + String.valueOf((dataUntilNextHeader / BytesPerMinute)) + " minute(s)");
+                Log.d(TAG, "TIMESTAMP: " + DateFormat.getDateTimeInstance().format(timestamp.getTime()) + " magic byte: " + dataUntilNextHeader);
+
+                byte[] ackTime = calendarToRawBytes(timestamp);
+
+                /*byte[] ackChecksum = new byte[]{
+                        (byte) (dataUntilNextHeader & 0xff),
+                        (byte) (0xff & (dataUntilNextHeader >> 8))
+                };*/
+
+                byte[] ackChecksum = new byte[]{
+                        (byte) (~dataUntilNextHeader & 0xff),
+                        (byte) (0xff & (~dataUntilNextHeader >> 8))
+                };
+
+                byte[] ack = new byte[]{
+                        COMMAND_CONFIRM_ACTIVITY_DATA_TRANSFER_COMPLETE,
+                        ackTime[0],
+                        ackTime[1],
+                        ackTime[2],
+                        ackTime[3],
+                        ackTime[4],
+                        ackTime[5],
+                        ackChecksum[0],
+                        ackChecksum[1]
+                };
+
+
+              // BluetoothGattCharacteristic bch = mGattService.getCharacteristic(UUID_CHAR_ACTIVITY_DATA);
+              // bch.setValue(ack);
+              // gatt.readCharacteristic(bch);
+                /*if (mGattService != null) {
+                    Log.i(TAG, "onServicesDiscovered UUID_SERVICE_MILI");
+
+                    BluetoothGattCharacteristic bch = mGattService.getCharacteristic(UUID_CHAR_CONTROL_POINT);
+                    if (bch != null) {
+                        byte[] GET_ACT = {6};
+                        bch.setValue(GET_ACT);
+                        Log.i(TAG, "BluetoothGattCharacteristic UUID_CHAR_CONTROL_POINT");
+                        //gatt.readCharacteristic(bch);
+                        gatt.writeCharacteristic(bch);
+                    }
+                }*/
+
+
+            }
 
             //Log.d(TAG, "getBatteryInfo result " + Arrays.toString(characteristic.getValue()));
 
@@ -187,12 +400,81 @@ public class MainActivity extends AppCompatActivity {
 
             byte[] data = characteristic.getValue();
 
+            /*if (data.length == 4) {
+                int steps = data[3] << 24 | (data[2] & 0xFF) << 16 | (data[1] & 0xFF) << 8 | (data[0] & 0xFF);
+                Log.d(TAG, "steps : " + steps);
+            }
+
             if (data.length == 2 && data[0] == 6) {
                 int heartRate = data[1] & 0xFF;
 
                 Log.d(TAG, "heartRate : " + heartRate);
+            }*/
+
+            if (data.length == 11 ) {
+                Log.d(TAG, "activity data meta  ");
+
+                int dataType = data[0];
+
+                GregorianCalendar timestamp = rawBytesToCalendar(data, 1);
+
+                int totalDataToRead = (data[7] & 0xff) | ((data[8] & 0xff) << 8);
+                totalDataToRead *= (dataType == MODE_REGULAR_DATA_LEN_MINUTE) ? BytesPerMinute : 1;
+                Log.d(TAG, "totalDataToRead : " + totalDataToRead);
+
+                // counter of this data block
+                int dataUntilNextHeader = (data[9] & 0xff) | ((data[10] & 0xff) << 8);
+                dataUntilNextHeader *= (dataType == MODE_REGULAR_DATA_LEN_MINUTE) ? BytesPerMinute : 1;
+                Log.d(TAG, "counter data block : " + dataUntilNextHeader);
+
+                Log.d(TAG,"total data to read: " + totalDataToRead + " len: " + (totalDataToRead / BytesPerMinute) + " minute(s)");
+
+                Log.d(TAG,"data to read until next header: " + dataUntilNextHeader + " len: " + String.valueOf((dataUntilNextHeader / BytesPerMinute)) + " minute(s)");
+                Log.d(TAG, "TIMESTAMP: " + DateFormat.getDateTimeInstance().format(timestamp.getTime()) + " magic byte: " + dataUntilNextHeader);
+
+            } else  {
+                Log.d(TAG, "activity data raw  ");
+
+                for (int i = 0; i < data.length; i += BytesPerMinute) {
+
+                    Log.d(TAG, "category:" + data[i] + ",intensity:" + (data[i + 1]& 0xff) + ",steps:" + (data[i + 2]& 0xff) + ",heartrate:" + (data[i + 3] & 0xff));
+                }
             }
 
+
+
+
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            Log.i("onCharacteristicWrite", descriptor.toString());
+
+          /*  BluetoothGattCharacteristic bch = mGattService.getCharacteristic(UUID_CHAR_ACTIVITY_DATA);
+            if (bch != null) {
+                Log.i(TAG, "BluetoothGattCharacteristic UUID_CHAR_CONTROL_POINT");
+
+                gatt.setCharacteristicNotification(bch, true);
+                BluetoothGattDescriptor descriptornoti = bch.getDescriptor(UUID_DESCRIPTOR_UPDATE_NOTIFICATION);
+                descriptornoti.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                gatt.writeDescriptor(descriptornoti);
+
+            }*/
+
+            BluetoothGattCharacteristic bchg = mGattService.getCharacteristic(UUID_CHAR_CONTROL_POINT);
+            if (bchg != null) {
+                byte[] GET_ACT = {6};
+                bchg.setValue(GET_ACT);
+                gatt.writeCharacteristic(bchg);
+            }
+        }
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            Log.i("onCharacteristicWrite", characteristic.toString());
+
+           // BluetoothGattCharacteristic bch = mGattService.getCharacteristic(UUID_CHAR_ACTIVITY_DATA);
+
+            //gatt.readCharacteristic(bch);
 
         }
     };
@@ -255,6 +537,33 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private GregorianCalendar rawBytesToCalendar(byte[] value, int offset) {
+        if (value.length - offset >= 6) {
+            GregorianCalendar timestamp = new GregorianCalendar(
+                    value[offset] + 2000,
+                    value[offset + 1],
+                    value[offset + 2],
+                    value[offset + 3],
+                    value[offset + 4],
+                    value[offset + 5]);
+
+            return timestamp;
+        }
+
+        return new GregorianCalendar();
+    }
+
+    private byte[] calendarToRawBytes(Calendar timestamp) {
+        return new byte[]{
+                (byte) (timestamp.get(Calendar.YEAR) - 2000),
+                (byte) timestamp.get(Calendar.MONTH),
+                (byte) timestamp.get(Calendar.DATE),
+                (byte) timestamp.get(Calendar.HOUR_OF_DAY),
+                (byte) timestamp.get(Calendar.MINUTE),
+                (byte) timestamp.get(Calendar.SECOND)
+        };
     }
 
     private void BlueToothDirect() {
